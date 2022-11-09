f65a907037ed3b4beaf743091818d51666885e3b
Zarina Nazimidinova <zarinanazimidinova229@gmail.com>
2022-11-09 18:38:50 +0600

    dz-1-4.js commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//dz-1
+
+// let count = 0;
+// function recursive() {
+//     count++;
+//     console.log(count, 'array');
+//     if (count === 3) {
+//         return
+//     }else{
+//         return recursive();
+//     }
+// } 
+// recursive()
+
+
+//dz-2
+
+// let array = [4,5,6]
+// array.push = [7,8,9,10]
+// array.unshift = [1,2,3]
+// console.log(array);
+
+
+//dz-3
+
+// let array = [1,3,5];
+// let lastElem = array.pop();
+// console.log(lastElem);
+
+
+//dz-4
+
+// function countChar () {
+// let countChar = 'loremipsumdolor'
+// console.log(countChar.match(/o/g))
+// }
+// countChar();
